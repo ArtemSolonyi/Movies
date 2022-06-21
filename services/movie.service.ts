489759859867738caa21a21df73d80a524613d33
ArtemSolonyi <igorartem123@gmail.com>
2022-06-22 02:46:02 +0300

    Create Category model

--- services/movie.service.ts
+++ services/movie.service.ts
@@ -1,16 +1,15 @@
-import {Request,Response} from "express";
+import {Request, Response} from "express";
 import {Movie} from '../models/Movie'
 
 
-
 export class MovieService {
-    createMovie = async (req:Request, res:Response) => {
-        const {title, description} = req.body
+    createMovie = async (req: Request, res: Response) => {
+        const {title, description, category} = req.body
         const movie = await Movie.create(title, description)
         return res.status(200).json({"movie": movie})
     }
 
-    getMovie = async (req:Request, res:Response) => {
+    getMovie = async (req: Request, res: Response) => {
         const id = req.query.id
         const movie = await Movie.findOne({_id: id})
         if (!movie) {
@@ -19,13 +18,13 @@ export class MovieService {
         return res.status(200).json(movie)
     }
 
-    updateMovie = async (req:Request, res:Response) => {
+    updateMovie = async (req: Request, res: Response) => {
         const {id, title, description, rating} = req.body
         const movie = Movie.updateOne(id, title, description, rating)
         return res.status(200).json({"movie": movie})
     }
 
-    deleteMovie = async (req:Request, res:Response) => {
+    deleteMovie = async (req: Request, res: Response) => {
         const id = req.query.id
         const movie = Movie.deleteOne({_id: id})
         if (!movie) {
